4c1a93c78700501f2df0764318f731cf651e0d9b
Joao Carvalho <jmmtcarvalho@gmail.com>
2015-05-11 16:13:12 +0100

    Correct tests - experiment firefox

--- tests/integration/ci-protractor-conf.js
+++ tests/integration/ci-protractor-conf.js
@@ -26,9 +26,10 @@ exports.config = {
         'browserName': 'firefox',
         'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
         'build': process.env.TRAVIS_BUILD_NUMBER,
-        'name': 'angular-typeaheadjs e2e tests'/*,
+        'name': 'angular-typeaheadjs e2e tests',
         'version': '36',
-        'selenium-version': '2.45.0'*/
+        'selenium-version': '2.45.0',
+        'platform': 'LINUX'
     }/*, {
         'browserName': 'safari',
         'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
