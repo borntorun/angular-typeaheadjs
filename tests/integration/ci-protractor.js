86ef26d7e287756a17b36ca3672ff38f9e132930
Joao Carvalho <jmmtcarvalho@gmail.com>
2015-05-12 00:04:04 +0100

    Correct tests - experiment

---
+++ tests/integration/ci-protractor.js
@@ -0,0 +1,28 @@
+var env = require('./env.js');
+
+exports.config = {
+    sauceUser: process.env.SAUCE_USERNAME,
+    sauceKey: process.env.SAUCE_ACCESS_KEY,
+
+    framework: 'jasmine',
+
+    specs: [
+        '../e2e/local_spec.js'
+    ],
+
+    multiCapabilities: require('./' + process.env.BROWSER).config,
+
+    baseUrl: env.baseUrl,
+    rootElement: 'body',
+
+    // Up the timeouts for the slower browsers (IE, Safari).
+    //allScriptsTimeout: 30000,
+    //getPageTimeout: 30000,
+
+    jasmineNodeOpts: {
+        isVerbose: true,
+        showTiming: true,
+        showColors: true,
+        defaultTimeoutInterval: 90000
+    }
+};
---
+++ tests/integration/ie.js
@@ -0,0 +1,20 @@
+exports.config = [
+    {
+        'browserName': 'internet explorer',
+        'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+        'build': process.env.TRAVIS_BUILD_NUMBER,
+        'name': 'angular-typeaheadjs e2e tests',
+        'version': '11',
+        'selenium-version': '2.45.0',
+        'platform': 'Windows 7'
+    },
+    {
+        'browserName': 'internet explorer',
+        'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+        'build': process.env.TRAVIS_BUILD_NUMBER,
+        'name': 'angular-typeaheadjs e2e tests',
+        'version': '10',
+        'selenium-version': '2.45.0',
+        'platform': 'Windows 7'
+    }
+];
