635a9476a9324009e7c2aac60dcf6c5e658d07d9
Joao Carvalho <jmmtcarvalho@gmail.com>
2015-05-12 20:01:57 +0100

    Alter page for v0.6.0

--- js/app.js
+++ js/app.js
@@ -1,28 +1,43 @@
-(function() {
+(function () {
     'use strict';
-
     angular.module('appdemo', ['angularTypeaheadjs'])
         .controller('RemotePrefetchCtrl', [ '$scope', RemotePrefetchCtrl ]);
-
-    function RemotePrefetchCtrl() {
+    function RemotePrefetchCtrl($scope) {
         var vm = this;
         vm.itemonSelected = [];
-        vm.prefetch = 'data/countries.json'
-        vm.remote = 'data/%QUERY.json';
-        vm.onSelected = function (item) {
-            for(var i = vm.itemonSelected.length - 1; i >= 0; i--) {
-                if(vm.itemonSelected[i] === item.name) {
-                    vm.itemonSelected.splice(i, 1);
+        vm.ttoptions = {
+            prefetch: 'data/countries.json',
+            remote: 'data/%QUERY.json',
+            name: 'countries',
+            display: 'name',
+            minLength: 1,
+            classNames: {input: 'inputclass'},
+            sufficient: 10,
+            limit: 5
+        };
+        vm.options = {
+            clear: true
+        }
+        $scope.$on('typeahead:cursorchange', function (ev, item) {
+            $scope.$apply(function () {
+                vm.itemonCursorChanged = item[1].name;
+            });
+        });
+        vm.onSelected = function (event, item) {
+            $scope.$apply(function () {
+                for (var i = vm.itemonSelected.length - 1; i >= 0; i--) {
+                    if (vm.itemonSelected[i] === item.name) {
+                        vm.itemonSelected.splice(i, 1);
+                    }
                 }
-            }
-            vm.itemonSelected.push(item.name);
-            vm.itemonCursorChanged = '';
+                vm.itemonSelected.push(item.name);
+                vm.itemonCursorChanged = '';
+            });
         };
-        vm.onCursorChanged = function(item) {
-            vm.itemonCursorChanged = item.name;
-        };
-        vm.onClosed = function() {
-            vm.itemonCursorChanged = '';
+        vm.onClosed = function () {
+            $scope.$apply(function () {
+                vm.itemonCursorChanged = '';
+            });
         };
     }
 }());
