e5e1bc2aa5ec0f4e97b19bb38f4398ba50f7557d
Joao Carvalho <jmmtcarvalho@gmail.com>
2015-05-12 09:15:39 +0100

    Correct e2e tests - experiment browser ie10

--- tests/integration/ci-protractor-conf.js
+++ tests/integration/ci-protractor-conf.js
@@ -20,14 +20,7 @@ exports.config = {
         'chromedriver-version': '2.14',
         //'platform': 'OS X 10.9'
         'platform': 'LINUX'*/
-    }, {
-        'browserName': 'firefox',
-        'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
-        'build': process.env.TRAVIS_BUILD_NUMBER,
-        'name': 'angular-typeaheadjs e2e tests',
-        'version': '36'/*,
-        'selenium-version': '2.45.0'*/
-    }, {
+    }, , {
         'browserName': 'safari',
         'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
         'build': process.env.TRAVIS_BUILD_NUMBER,
